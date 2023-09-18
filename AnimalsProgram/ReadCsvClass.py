95cfe5a65acf6298229afe2c72c122da99df76f8
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-09-18 16:29:04 +0300

    load data from csv, new class write data to csv

--- AnimalsProgram/ReadCsvClass.py
+++ AnimalsProgram/ReadCsvClass.py
@@ -17,9 +17,27 @@ class ReadCsv(AnimalsMainClass):
             result_list = [dict(zip(headers, i)) for i in reader_csv]
         return result_list
 
+    def put_data(self, data_list=None, file_name=None):
+        if not file_name:
+            file_name = self.file_name
+        import csv
+        with open(file_name, mode='w', encoding="utf-8-sig") as file:
+            for data_dict in data_list:
+                headers = data_dict.keys()
+                writer_csv = csv.DictWriter(file, headers)
+                writer_csv.writeheader()
+                writer_csv.writerow(data_dict)
+        return True
+
+
 
 if __name__ == '__main__':
-    reader = ReadCsv()
-    print(reader.get_data())
+    connector_csv = ReadCsv()
+    data = connector_csv.get_data()
+    print("data = ", data)
+    # new_data = data.pop()
+    # print(new_data)
+    # connector_csv.put_data([new_data])
+
 
 
